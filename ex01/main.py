2533497ed27666a7853aaad62266e3948c000ed7
LuxoJr22 <mathisbodu83@gmail.com>
2025-01-02 19:49:29 +0100

    ex01

--- ex01/main.py
+++ ex01/main.py
@@ -1,14 +1,25 @@
 def adder(a, b):
-    carry = a & b
-    result = a ^ b
-    while(carry != 0):
-        shiftedcarry = carry << 1
-        carry = result & shiftedcarry
-        result = result ^ shiftedcarry
-    return result
+	carry = a & b
+	result = a ^ b
+	while(carry != 0):
+		shiftedcarry = carry << 1
+		carry = result & shiftedcarry
+		result = result ^ shiftedcarry
+	return result
+
+def multiplier(a, b):
+	factor1 = a
+	factor2 = b
+	result = 0
+	while(factor2 != 0):
+		if (factor2 & 1):
+			result = adder(result, factor1)
+		factor2 = factor2 >> 1
+		factor1 = factor1 << 1
+	return result
 
 def main():
-	print(adder(4, 4))
+	print(multiplier(4, 1))
 	return 0
 
 if __name__ == "__main__":
